410acdea0d3b0ad4297ded792f2a249e597a396f
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-04-19 06:18:13 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,38 @@
+
+// Struct TreeNode is in-built in the solution file on leetcode.com. 
+// When running the code on the website, do not include this struct.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    
+    TreeNode* current = new TreeNode();
+
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        TreeNode* newRoot = current;
+        inOrder(root);
+        return newRoot->right;
+    }
+
+private:
+    void inOrder(TreeNode* root) {
+        if (root != nullptr) {
+            inOrder(root->left);
+            current->right = root;
+            current->right->left = nullptr;
+            current = current->right;
+            inOrder(root->right);
+        }
+    }
+};
